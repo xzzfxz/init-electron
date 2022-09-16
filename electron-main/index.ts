a991c12212bd67715bad902606f03c4a832e420b
李冲 <chongli@sumscope.com>
2022-09-16 09:45:06 +0800

    feat: 初始化electron

---
+++ electron-main/index.ts
@@ -0,0 +1,46 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+import path from 'path';
+
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      // 是否开启隔离上下文
+      contextIsolation: false,
+      // 渲染进程使用Node API
+      nodeIntegration: true,
+      // 需要引用js(后缀名改为js，源文件可以用ts)文件，不能用ts
+      preload: path.join(__dirname, '../electron-preload/index.js'),
+    },
+  });
+  // 如果打包了，渲染index.html
+  if (app.isPackaged) {
+    win.loadFile(path.join(__dirname, '../index.html'));
+  } else {
+    win.webContents.openDevTools();
+    const url = 'http://localhost:5173';
+    win.loadURL(url);
+  }
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (!BrowserWindow.getAllWindows().length) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+ipcMain.on('window-new', (e: Event, data: string) => {
+  console.log(data);
+});
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import electron from 'vite-plugin-electron';
+import electronRenderer from 'vite-plugin-electron-renderer';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    electron({
+      // 主进程文件
+      main: { entry: 'electron-main/index.ts' },
+      // 预加载文件
+      preload: {
+        input: path.join(__dirname, 'electron-preload/index.ts'),
+        vite: {
+          build: {
+            sourcemap: 'inline',
+            outDir: 'dist/electron-preload',
+          },
+        },
+      },
+    }),
+    electronRenderer(),
+  ],
+  build: {
+    // 默认情况下，若 outDir 在 root 目录下，则 Vite 会在构建时清空该目录
+    emptyOutDir: false,
+  },
+});
